627175333e72fcd220fb515dfea9db3ac5c63239
Aamna <56640566+najmia@users.noreply.github.com>
2025-09-16 18:40:24 +0200

    Initial commit

---
+++ infra/outputs.tf
@@ -0,0 +1,34 @@
+output "bedrock_role_arn" {
+  description = "ARN of the Bedrock agent role"
+  value       = module.bedrock_role.role_arn
+}
+
+output "knowledge_base_id" {
+  description = "ID of the knowledge base"
+  value       = module.kb_stack.knowledge_base_id
+}
+
+output "guardrail_id" {
+  description = "ID of the guardrail"
+  value       = module.guardrail.guardrail_id
+}
+
+output "user_pool_id" {
+  description = "ID of the Cognito user pool"
+  value       = module.cognito.user_pool_id
+}
+
+output "client_id" {
+  description = "ID of the Cognito client"
+  value       = module.cognito.user_pool_client_id
+}
+
+output "data_source_id" {
+  description = "ID of the knowledge base data source"
+  value       = module.kb_stack.data_source_id
+}
+
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket for knowledge base"
+  value       = module.kb_stack.s3_bucket_name
+}
